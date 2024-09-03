ef46c1427a7f34254517153aac14fddd4c9800ae
Jaewung Lee <james@jamesnet.dev>
2024-09-03 21:07:59 +0800

    Add RiotStarButton

---
+++ src/Leagueoflegends.Support/UI/Units/RiotStarButton.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Leagueoflegends.Support.UI.Units;
+
+public class RiotStarButton : Button
+{
+    public RiotStarButton()
+    {
+        DefaultStyleKey = typeof(RiotStarButton);
+    }
+}
